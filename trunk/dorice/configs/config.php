1cc70cad8fa772b25a11f9df24bf814994d31286
leihuaishui@gmail.com <leihuaishui@gmail.com@8ff9c99d-4401-6823-cda4-1b32db97959a>
2013-04-02 08:33:20 +0000

    前台样例

---
+++ trunk/dorice/configs/config.php
@@ -0,0 +1,12 @@
+<?php
+$this->template_dir=BASE_PATH.'templates/';
+$this->compile_dir=BASE_PATH.SMARTY_PATH.'templates_c/';
+$this->config_dir=BASE_PATH.'configs/';
+$this->cache_dir=BASE_PATH.'cache/';
+$this->left_delimiter = '<{';
+$this->right_delimiter = '}>';
+$this->db_host='127.0.0.1';
+$this->db_name='das';
+$this->db_user='root';
+$this->db_pass='';
+?>
