3c19425e03d1eb2685ae7ebfad6b6b3e90c56da9
leihuaishui <leihuaishui@gmail.com>
2013-04-02 08:30:36 +0000

    前台样例

---
+++ dorice/modules/topic.mod.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Description of topic
+ *
+ * @author kappa
+ */
+if(!defined('SMARTY'))
+{
+    exit('invalid request');
+}
+class ModSmarty extends Smarty{
+    var $code;
+    var $mod;
+    function ModSmarty($mod,$code){
+        $this->config();
+        $this->code=$code;
+        $this->mod=$mod;
+        $this->assign('mod', $this->mod);
+        $this->assign('code', $this->code);
+        $this->Execute();
+    }
+    function Execute(){
+        switch ($this->code){
+            case 'search':
+                $this->search();
+                break;
+            default :
+                $this->main();
+                break;
+        }
+    }
+    function main(){
+        $this->display('topic_index.html');
+    }
+    function search(){
+        $key=  getUrl('key');
+        $this->assign('key',$key);
+        $this->display('topic_search_index.html');
+    }
+}
+
+?>
