9d1399ead054e1ef7313fa05893f4317078a76d3
leihuaishui <leihuaishui@gmail.com>
2013-04-15 09:08:44 +0000

    前台样例

---
+++ dorice/ajax/topic.mod.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Description of topic
+ *
+ * @author kappa
+ */
+if(!defined('SMARTY'))
+{
+    exit('invalid request');
+}
+class ModSmarty extends Smarty{
+    var $code;
+    var $mod;
+    function ModSmarty($mod,$code){
+        $this->config();
+        $this->code=$code;
+        $this->mod=$mod;
+        $this->assign('mod', $this->mod);
+        $this->assign('code', $this->code);
+        $this->Execute();
+    }
+    function Execute(){
+        switch ($this->code){
+            case 'showLoginPanel':
+                $this->showLoginPanel();
+                break;
+            default :
+                $this->main();
+                break;
+        }
+    }
+    function main(){
+        exit;
+    }
+    function showLoginPanel(){    
+        $this->display('topic_show_login.html');
+    }
+}
+
+?>
