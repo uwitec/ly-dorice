215bac10ad52d7a3526ddb16af7f070d8c58a5af
leihuaishui@gmail.com <leihuaishui@gmail.com@8ff9c99d-4401-6823-cda4-1b32db97959a>
2013-04-16 08:51:57 +0000

    前台样例

--- trunk/dorice/include/global.func.php
+++ trunk/dorice/include/global.func.php
@@ -4,7 +4,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-function getUrl($type){
+function Get($type){
     if(isset($_GET[$type])){
         $option=$_GET[$type];
     }else{
@@ -12,5 +12,19 @@ function getUrl($type){
     }
     return $option;
 }
-
+function Post($type){
+    return $_POST[$type];
+}
+function Session($type,$value){
+    $_SESSION[$type]=$value;
+}
+function GetSession($type){
+    return $_SESSION[$type];
+}
+function Query($sql){
+    return mysql_query($sql);
+}
+function GetRow($query){
+    return mysql_fetch_array($query);
+}
 ?>
