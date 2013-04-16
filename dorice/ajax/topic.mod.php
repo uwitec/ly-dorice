4bf60eca9e9fe647e7322f15850b4f7284b0b3cc
leihuaishui <leihuaishui@gmail.com>
2013-04-16 08:51:34 +0000

    前台样例

--- dorice/ajax/topic.mod.php
+++ dorice/ajax/topic.mod.php
@@ -24,6 +24,15 @@ class ModSmarty extends Smarty{
             case 'showLoginPanel':
                 $this->showLoginPanel();
                 break;
+            case 'showName':
+                $this->showName();
+                break;
+            case 'showCode':
+                $this->showCode();
+                break;
+            case 'register':
+                $this->member_register();
+                break;
             default :
                 $this->main();
                 break;
@@ -34,6 +43,42 @@ class ModSmarty extends Smarty{
     }
     function showLoginPanel(){    
         $this->display('topic_show_login.html');
+    }
+    function showName(){
+        $name=trim(Post('name'));
+        if($name!=""){
+            $sql="select * from ly_users where username='{$name}'";
+            $query=  Query($sql);
+            $row=GetRow($query);
+            if($row){
+                echo '1';
+            }else{
+                echo '0';
+            }
+        }
+    }
+    function showCode(){
+        $code=Post('code');
+        $check=  GetSession('check_num');
+        if($code==$check){
+            echo '0';
+        }else{
+            echo '1';
+        }
+    }
+    function member_register(){
+        $name=Post('name');
+        $pass=md5(Post('pass'));
+        $email=Post('email');
+        $re_ip=$_SERVER["REMOTE_ADDR"];
+        $now_ip=$_SERVER["REMOTE_ADDR"];
+        $sql="insert into ly_users (username,nickname,password,email,level,registe_ip,last_login_ip,login_times,online_state) values('{$name}','{$name}','{$pass}','{$email}','1','{$re_ip}','{$now_ip}','1','1')";
+        $query=  Query($sql);
+        if($query){
+            echo '0';
+        }else{
+            echo '1';
+        }
     }
 }
 
