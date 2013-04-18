511590f51199e5ab7483f73838d2487c6f2bf289
leihuaishui@gmail.com <leihuaishui@gmail.com@8ff9c99d-4401-6823-cda4-1b32db97959a>
2013-04-18 03:58:34 +0000

    前台样例

--- trunk/dorice/modules/topic.mod.php
+++ trunk/dorice/modules/topic.mod.php
@@ -11,12 +11,18 @@ if(!defined('SMARTY'))
 class ModSmarty extends Smarty{
     var $code;
     var $mod;
+    var $ID;
     function ModSmarty($mod,$code){
         $this->config();
+        $id=  GetID('id');
+        $this->ID=$id;
         $this->code=$code;
         $this->mod=$mod;
+        $this->assign('ID',$this->ID);
         $this->assign('mod', $this->mod);
         $this->assign('code', $this->code);
+        $member=member($this->ID);
+        $this->assign('member',$member);
         $this->Execute();
     }
     function Execute(){
@@ -30,10 +36,11 @@ class ModSmarty extends Smarty{
         }
     }
     function main(){
+        
         $this->display('topic_index.html');
     }
     function search(){
-        $key=  Get('key');
+        $key=Get('key');
         $this->assign('key',$key);
         $this->display('topic_search_index.html');
     }
